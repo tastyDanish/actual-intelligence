1b2e022e89d96f7c2af332ff0e528550dcb1c479
Peter Lansdaal <peter@gmail.com>
2025-03-27 13:31:36 -0700

    fix: missing home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,25 @@
-import Hero from "@/components/hero";
-import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
-import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default async function Home() {
   return (
     <>
-      <Hero />
       <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
+        <div>Welcome to actual intelligence. where the AI is U</div>
+        <div className="flex gap-2">
+          <Button
+            asChild
+            size="sm"
+            variant={"outline"}>
+            <Link href="/sign-in">Sign in</Link>
+          </Button>
+          <Button
+            asChild
+            size="sm"
+            variant={"default"}>
+            <Link href="/sign-up">Sign up</Link>
+          </Button>
+        </div>
       </main>
     </>
   );
