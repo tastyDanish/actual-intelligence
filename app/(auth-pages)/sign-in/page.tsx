5c327ae1fb76290db2dfa0edd37d352236fc40dd
Peter Lansdaal <peter@gmail.com>
2025-03-27 15:03:24 -0700

    update: sign in flow

--- app/(auth-pages)/sign-in/page.tsx
+++ app/(auth-pages)/sign-in/page.tsx
@@ -8,23 +8,28 @@ import Link from "next/link";
 export default async function Login(props: { searchParams: Promise<Message> }) {
   const searchParams = await props.searchParams;
   return (
-    <form className="flex-1 flex flex-col min-w-64">
+    <form className="flex-1 flex flex-col min-w-64 pt-20">
       <h1 className="text-2xl font-medium">Sign in</h1>
       <p className="text-sm text-foreground">
         Don't have an account?{" "}
-        <Link className="text-foreground font-medium underline" href="/sign-up">
+        <Link
+          className="text-foreground font-medium underline"
+          href="/sign-up">
           Sign up
         </Link>
       </p>
       <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
         <Label htmlFor="email">Email</Label>
-        <Input name="email" placeholder="you@example.com" required />
+        <Input
+          name="email"
+          placeholder="you@example.com"
+          required
+        />
         <div className="flex justify-between items-center">
           <Label htmlFor="password">Password</Label>
           <Link
             className="text-xs text-foreground underline"
-            href="/forgot-password"
-          >
+            href="/forgot-password">
             Forgot Password?
           </Link>
         </div>
@@ -34,7 +39,9 @@ export default async function Login(props: { searchParams: Promise<Message> }) {
           placeholder="Your password"
           required
         />
-        <SubmitButton pendingText="Signing In..." formAction={signInAction}>
+        <SubmitButton
+          pendingText="Signing In..."
+          formAction={signInAction}>
           Sign in
         </SubmitButton>
         <FormMessage message={searchParams} />
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default async function Home() {
   return (
     <>
-      <main className="flex-1 flex flex-col gap-6 px-4">
+      <main className="flex-1 flex flex-col gap-6 px-4 pt-20 items-center">
         <div>Welcome to actual intelligence. where the AI is U</div>
         <div className="flex gap-2">
           <Button
