ce39f085b8fe74b74586022f226abeb600c353ab
Peter Lansdaal <peter@gmail.com>
2025-03-28 10:01:14 -0700

    update: sidebar closes on mobile with new conversation

--- components/conversation-list/new-conversation.tsx
+++ components/conversation-list/new-conversation.tsx
@@ -1,13 +1,20 @@
 "use client";
 import { useConversation } from "@/hooks/conversations-provider";
 import { Button } from "../ui/button";
+import { useSidebar } from "../ui/sidebar";
 
 export const NewConversation = () => {
+  const { setOpenMobile, isMobile } = useSidebar();
   const { setConversationId } = useConversation();
+
+  const onClickHandler = (conversationId: string | null) => {
+    setConversationId(conversationId);
+    if (isMobile) setOpenMobile(false);
+  };
   return (
     <Button
       className="w-full"
-      onClick={() => setConversationId(null)}>
+      onClick={() => onClickHandler(null)}>
       new conversation
     </Button>
   );
